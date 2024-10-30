3918dad2f2e776f3bbb9b828e17a9c8a30b625a4
Basem-Mouner <bmr.audi@gmail.com>
2024-10-30 14:07:45 +0300

    sec commit

--- js-files/bonus.js
+++ js-files/bonus.js
@@ -1,5 +1,20 @@
 'use strict'
 
+/*
+Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+The three functions are:
+
+increment() increases the current value by 1 and then returns it.
+decrement() reduces the current value by 1 and then returns it.
+reset() sets the current value to init and then returns it.
+
+Constraints:
+
+-1000 <= init <= 1000
+0 <= calls.length <= 1000
+calls[i] is one of "increment", "decrement" and "reset"
+*/
 
 //____________________function return object with 3 methods inc resrt and dec ___________________________________________________________
 function createCounter(init) {
